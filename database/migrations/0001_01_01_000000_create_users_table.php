e1c1ed3bf646274a370ad7d47626e52c13f66453
Modather-7 <modatherelbeltagy@std.mans.edu.eg>
2026-04-02 16:03:41 +0200

    added new database migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone_number', 11)->unique();
+            $table->text('address');
+            $table->enum('branch', ['elmahalla', 'tanta', 'elmansoura']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ database/migrations/2026_04_01_130057_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('branch_id')->constrained()->onDelete('cascade');
+            $table->decimal('total_price', 10, 2);
+            $table->enum('order_status', ['received', 'preparing', 'on_the_way', 'delivered'])->default('received');
+            $table->enum('payment_method', ['cash_on_delivery', 'visa', 'mobile_wallet']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
