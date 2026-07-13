f3a87bb440626f44661e0e0b86da6f98e0e65aca
Modather-7 <modatherelbeltagy@std.mans.edu.eg>
2026-07-13 15:46:22 +0300

    Load vite assets in frontend layout

--- resources/views/front/branch.blade.php
+++ resources/views/front/branch.blade.php
@@ -8,6 +8,7 @@
 
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="foodgrids-branch-body grid-bg">
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -15,6 +15,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;800&display=swap" rel="stylesheet">
 
     @stack('styles')
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="d-flex flex-column min-vh-100">
 
